1cee5e961df388be3f9037438e0bd81d2c5af078
Vessela Marinova <vessela.v.marinova@gmail.com>
2024-11-17 15:48:10 +0200

    ngIf for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-udemy-the-complete-guide-2024-recipe-book-moudles';
+
+  loadedFeature='recipe';
+
+  onNavigate(feature:string){
+    console.log(feature);
+    this.loadedFeature=feature;
+
+  }
 }
